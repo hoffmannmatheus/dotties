433edfa8fd291e5e3514c1ca85d0a7411ce3439f
Matheus Hoffmann Silva <hoffmann.matheus@gmail.com>
2014-06-02 14:30:04 -0300

    Script update.

--- install.sh
+++ install.sh
@@ -9,14 +9,21 @@ sudo apt-get -qy install konsole
 
 # dotties
 echo ' copying dotties...'
+pushd . > /dev/null
+cd dots
+
 cp .bashrc ~/.bashrc
 cp .vimrc ~/.vimrc
+
 tar -zxvf .vim.tgz
 mv .vim ~/
 rm -rf .vim
+
 tar -zxvf kde.tgz
 mkdir -p ~/kde/share/apps/konsole
 mv kde/* ~/kde/share/apps/konsole/
 rm -rf kde
+
+popd > /dev/null
 
 echo 'Done!'
---
+++ pack.sh
@@ -0,0 +1,24 @@
+#/bin/bash
+
+# pack dots
+echo 'packing dotties...'
+
+source=$(pwd)
+pushd . > /dev/null
+cd ~
+
+# vim
+echo ' packing vim...'
+cp .vimrc $source/dots/
+tar -zcvf .vim.tgz .vim
+mv .vim.tgz $source/dots/
+
+# bash
+echo ' packing bashrc...'
+cp .bashrc $source/dots/
+
+popd > /dev/null
+
+echo 'done.'
+git status
+
