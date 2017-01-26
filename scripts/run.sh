1f6aaa159cea70dc3e7a6af4fd68b5f9b9142ce9
Matt Silva <matt.silva@kiswe.com>
2017-01-25 23:57:09 -0500

    Added forever runner.

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-#DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-sudo node_modules/.bin/http-server public -p 80 -d False
+sudo node_modules/.bin/forever start -c node_modules/.bin/http-server public -p 80 -d False
